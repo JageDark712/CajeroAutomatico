b1abdea3185ea025e712f92959777335bf9d85c7
Jager <darkliner2013@gmail.com>
2022-03-10 19:53:59 -0500

    Add project files.

---
+++ CajeroAutomatico/Cajero.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroAutomatico
+{
+    internal class Cajero
+    {
+        static void Main(string[] args)
+        {
+            string nom, ape, id, tel;
+            double sal;
+            int clave, cuentaC;
+
+            Console.WriteLine("Ingrese sus nombres: ");
+            nom = Console.ReadLine();
+            Console.WriteLine("Ingrese sus apellidos: ");
+            ape = Console.ReadLine();
+            Console.WriteLine("Ingrese su id: ");
+            id = Console.ReadLine();
+            Console.WriteLine("Ingrese su teléfono: ");
+            tel = Console.ReadLine();
+            Console.WriteLine("Indique cual será el depósito inicial a la cuenta bancaria: ");
+            sal = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la clave de su cuenta: ");
+            clave = int.Parse(Console.ReadLine());
+            Random cuenta = new Random();
+            cuentaC = cuenta.Next(10000, 99999);
+
+            Usuario cliente = new Usuario(nom, ape, id, tel, clave, sal, cuentaC);
+
+            Console.WriteLine("Su cuenta ha sido creada exitosamente.\nPor favor pulse enter para continuar.");
+            Console.ReadKey();
+
+            int resp;
+
+            Console.WriteLine("\n \n -------------------------------- \nBienvenid@ al cajero electrónico de su Banco MiDinero.\n -------------------------------- \n\n");
+
+            do
+            {
+                Console.WriteLine("\nPor favor ingrese la opción que desea realizar en su cuenta, siendo: ");
+                Console.WriteLine("\n1)Depósito. \n2)Retiro. \n3)Consultar saldo. \n4)Mostar toda la información de la cuenta. \n5)Transferir a otra cuenta. \n6)Cambio de clave. \n7)Salir.\n");
+                resp = int.Parse(Console.ReadLine());
+                double monto = 0;
+                int aux = 0, claveNew=0;
+                switch (resp)
+                {
+                    case 1:
+                        Console.WriteLine("\nIngrese el valor a depositar: ");
+                        monto = double.Parse(Console.ReadLine());
+                        cliente.Deposito(monto);
+                        
+                        break;
+                    case 2:
+                        Console.WriteLine("\nIngrese el valor a retirar: ");
+                        monto = double.Parse(Console.ReadLine());
+                        cliente.Retirar(monto);
+                        
+                        break;
+                    case 3:
+                        cliente.ConsultaSaldo();
+                        
+                        break;
+                    case 4:
+                        Console.WriteLine("\nLos datos de su cuenta son: \n" + cliente.ToString());
+                        
+                        break;
+                    case 5:
+                        Console.WriteLine("\nIngrese el valor a transferir: ");
+                        monto = double.Parse(Console.ReadLine());
+                        Console.WriteLine("\nIngrese el número de cuenta al que desea transferir: ");
+                        aux = int.Parse(Console.ReadLine());
+                        cliente.Transferencia(monto, aux);
+                        
+
+                        break;
+                    case 6:
+                        //Ingresar cambiar clave
+                        Console.WriteLine("Ingrese la clave antigua.");
+                        aux = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese la nueva clave.");
+                        claveNew = int.Parse(Console.ReadLine());
+                        cliente.CambiarClave(aux, claveNew);
+
+                        break;
+                    case 7:
+                        Console.WriteLine("\nGracias por usar nuestros servicios. Hasta pronto.");
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("\nOpción incorrecta.");
+                        resp = 0;
+                        
+                        break;
+                }
+            } while (resp < 7);
+        }
+    }
+}
---
+++ CajeroAutomatico/Usuario.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace CajeroAutomatico
+{
+    public class Usuario
+    {
+
+        private string nombre { get; set; }
+        private string apellido { get; set; }
+        private string id { get; set; }
+        private string tel { get; set; }
+        private int clave { get; set; }
+        private double saldo { get; set; }
+        private int cuenta { get; set; }
+
+        public Usuario(string nombrePa, string apellidoPa, string idPa, string telPa, int clavePa, double saldoPa, int cuentaPa)
+        {
+            nombre = nombrePa;
+            apellido = apellidoPa;
+            id = idPa;
+            tel = telPa;
+            clave = clavePa;
+            saldo = saldoPa;
+            cuenta = cuentaPa;
+
+        }
+
+        public int CambiarClave(int clavePa, int ClaveNew)
+        {
+            if (clavePa == clave)
+            {
+                clave = ClaveNew;
+            }
+            else
+            {
+                Console.WriteLine($"La clave antigua ingresada es incorrecta! ");
+            }
+            return clave;
+        }
+        public void ConsultaSaldo()
+        {
+            Console.WriteLine($"Saldo:{saldo}", 18);
+        }
+        public double Deposito(double montoPa)
+        {
+            saldo += montoPa;
+            Console.WriteLine("Depósito exitoso.");
+            return saldo;
+        }
+        public double Retirar(double montoPa)
+        {
+            if (montoPa <= saldo)
+            {
+                saldo -= montoPa;
+                Console.WriteLine("Retiro exitoso.");
+            }
+            else
+            { Console.WriteLine("Saldo insuficiente."); }
+            return saldo;
+        }
+        public double Transferencia(double montoPa, int numCuentaPa)
+        {
+            int cuentatransf = numCuentaPa;
+            if (montoPa <= saldo)
+            {
+                saldo -= montoPa;
+                Console.WriteLine($"Transferencia exitosa a la cuenta: {cuentatransf}");
+            }
+            else { Console.WriteLine("Saldo insuficiente, por favor intentelo de nuevo."); }
+            return saldo;
+        }
+
+        public override string ToString()
+        {
+            return ($"\nNombre usuario: {nombre}" +
+                    $"\nApellido: {apellido}" +
+                    $"\nIdentificacion: {id}" +
+                    $"\nTelefono: {tel}" +
+                    $"\nSaldo: {saldo}" +
+                    $"\nNumero de cuenta: {cuenta}"/* +
+                    $"\nClave: {clave}"*/);
+        }
+
+    }
+
+}
